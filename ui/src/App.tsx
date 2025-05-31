b51434c3e5b7647f248ec703c3bdeff771652a6d
Adham Salama <adhamsalama@zohomail.com>
2025-05-31 17:36:56 +0300

    ui: Add logo and change navbar color and update docs screenshots

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,27 +1,32 @@
-import { Container, Typography, AppBar, Toolbar, Button } from '@mui/material';
+import { Container, Box, Typography, AppBar, Toolbar, Button } from '@mui/material';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { TraceDetails } from './components/TraceDetails';
 import { SpanDetails } from './components/SpanDetails';
 import { SearchPage } from './components/SearchPage';
+import logo from '../../docs/assets/logo.png'; // adjust path if needed
 
 function App() {
-
   return (
     <Router>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ backgroundColor: '#2C6B6B' }}>
         <Toolbar>
-          <Typography
-            variant="h6"
-            component={Link}
-            to="/"
-            sx={{
-              flexGrow: 1,
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-          >
-            Nabatshy
-          </Typography>
+          <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
+            <Typography
+              variant="h6"
+              component={Link}
+              to="/"
+              sx={{
+                textDecoration: 'none',
+                color: 'inherit',
+              }}
+            >
+              <img
+                src={logo}
+                alt="Logo"
+                style={{ height: 50, marginRight: 16 }}
+              />
+            </Typography>
+          </Box>
           <Button color="inherit" component={Link} to="/search">
             Search
           </Button>
